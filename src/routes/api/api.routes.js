1d06fe657f3cd90534a74e0da2b5c238a7047b97
sangtqpc05237 <tranquocsangstg2004@gmail.com>
2024-05-27 18:33:58 +0700

    [ DOT-SV-3 ] - Hoàn thành Employees API

--- src/routes/api/api.routes.js
+++ src/routes/api/api.routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const productsApi = require('../../apis/products.api');
 const vouchersApi = require('../../apis/vouchers.api');
 const authAPI = require('../../apis/auth.api');
+const employeeAPI = require('../../apis/employees.api');
 
 router.use('/products', productsApi);
 router.use('/vouchers', vouchersApi);
 router.use('/auth', authAPI);
+router.use('/employees', employeeAPI);
 
 module.exports = router;
