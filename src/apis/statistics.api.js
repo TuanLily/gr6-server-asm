30f25806b3fb8dd3e8d4d20db332ffc780212b66
haohtpc05526 <trunghaopc23579@gmail.com>
2024-06-15 14:16:29 +0700

    [DOT-SV-7.1] - Chỉnh sửa thống kê API

--- src/apis/statistics.api.js
+++ src/apis/statistics.api.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const connection = require("../../index");
 
-router.get('/product-prices', (req, res) => {
-    const query = `
+router.get("/product-prices", (req, res) => {
+  const query = `
         SELECT
             MAX(price) AS maxPrice,
             MIN(price) AS minPrice,
@@ -11,16 +11,62 @@ router.get('/product-prices', (req, res) => {
         FROM products
     `;
 
-    connection.query(query, (err, results) => {
-        if (err) {
-            console.error('Error executing MySQL query: ' + err.stack);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error executing MySQL query: " + err.stack);
+      return res.status(500).json({ error: "Internal server error" });
+    }
 
-        const {minPrice, maxPrice, avgPrice} = results[0];
+    const { minPrice, maxPrice, avgPrice } = results[0];
 
-        res.json({message: "Success", minPrice: minPrice, maxPrice: maxPrice, avgPrice});
+    res.json({
+      message: "Success",
+      minPrice: minPrice,
+      maxPrice: maxPrice,
+      avgPrice,
     });
+  });
+});
+
+router.get("/bill-status", (req, res) => {
+  const query = `
+        SELECT
+            status,
+            COUNT(*) AS count
+        FROM bills
+        GROUP BY status
+    `;
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error executing MySQL query: " + err.stack);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    const statusCounts = results.reduce((acc, row) => {
+      acc[row.status] = row.count;
+      return acc;
+    }, {});
+
+    res.json({ message: "Success", statusCounts });
+  });
+});
+
+router.get("/count-products", (req, res) => {
+  const query = `
+      SELECT COUNT(*) as count FROM products
+  `;
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error executing MySQL query: " + err.stack);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    const { count } = results[0];
+
+    res.json({ message: "Success", count: count });
+  });
 });
 
 router.get('/count-products', (req, res) => {
